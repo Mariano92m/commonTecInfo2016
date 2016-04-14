a088cb46a9a2fb5f53692ea1573aa519c74b5763
Mariano92m <zeta007m@gmail.com>
2016-04-14 18:32:43 -0300

    Ejercicio 4 agregado y los nombres cambiados

---
+++ S2.py
@@ -0,0 +1,6 @@
+print ("Situacion 2")
+
+IVA = 0.21
+precio = int(input("Precio de produto:"))
+resultado = precio + (precio * IVA)
+print ("Precio mas IVA:",resultado)
--- Situacion2.py
+++
@@ -1,6 +0,0 @@
-print ("Situacion 2")
-
-IVA = 0.21
-precio = int(input("Precio de produto:"))
-resultado = precio + (precio * IVA)
-print ("Precio mas IVA:",resultado)
