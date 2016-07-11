31d6d4fb61cb43b15a7288a629fd7ec9afd6e0f4
Mariano92m <zeta007m@gmail.com>
2016-07-10 23:15:43 -0300

    Funcionalidad de la lista y el cazar listo

--- App/Chuwal 0.5/controllers/chuGame.py
+++ App/Chuwal 0.5/controllers/chuGame.py
@@ -1,27 +1,36 @@
 from gluon.serializers import loads_json
+import random
 #HTML
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 def chuMap():
-	return dict()
+    return dict()
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 def chuSculptInfo():
-    sculp= db.sculpture(request.args(0)) or redirect(URL('chuMap'))
+    idNum= getId(URL(args=request.args, vars=request.get_vars, host=True))
+    sculp= db.sculpture(idNum)
     return dict(sculp=sculp)
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
+#Controladores para la Lista
 def chuList():
     #en rows almaceno todos los registros de sculpture cuya ID sea mayor a cero
-    rows = db(db.place.id >0 ).select()
-    #devuelvo un diccionario "sculptures", el cual tiene asignados los elementos de "rows"
-    return dict(places=rows)
+    places=db().select(db.place.ALL,orderby=db.place.id)
+    return dict(places=places)
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 def chuHunt():
-    sculp= db.sculpture(request.args(0)) or redirect(URL('chuMap'))
+    idNum= getId(URL(args=request.args, vars=request.get_vars, host=True))
+    sculp= db.sculpture(idNum)
     return dict(sculp=sculp)
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 #Controladores para el Mapa
 @cache.action()
 def download():
     return response.download(request, db)
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 def call():
     """
     exposes services. for example:
@@ -30,10 +39,24 @@ def call():
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
     return service()
+
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 def somefunction():
     pic = db(db.sculpture).select().first().picture   #select first picture
     return dict(pic=pic)
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
+def getId(url):
+    cont=0
+    cn=0
+    for i in url:
+        if (i=='='):
+            cn=cont
+        cont=cont+1
+    numId=url[cn+1: len(url)]
+    return numId
+
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 #Cambia el color del marcador dependiendo si esta cazada o no
 def setMarkerColor(cazada):
     if(cazada=='S'):
@@ -41,6 +64,7 @@ def setMarkerColor(cazada):
     else:
         return "\Chuwal\static\images\PinRojo.png"
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 #Muestra una imagen dependiendo si esta cazada o no
 def showIMG(condition, imgC):
     if(imgC!="none"):
@@ -52,17 +76,19 @@ def showIMG(condition, imgC):
         ima ="https://media.giphy.com/media/pSpmpxFxFwDpC/giphy.gif"
     return ima
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 #Muestra un boton dependiendo si esta cazada o no
 def showBut(condition, imgC, sc):
     if(imgC!="none"):
         if(condition=='S'):
-            but='<a href="#" class="btn">Ver info</a>'
+            but='<a href="chuSculptInfo.html?args='+str(sc)+'" class="btn">Ver info</a>'
         else:
-            but='<a href="#" class="btn">Cazar</a>'
+            but='<a href="chuHunt.html?args='+str(sc)+'" class="btn">Cazar</a>' #56
     else:
         but='<a href="#" class="btn">Error</a>';
     return but
 
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 #Marcadores
 def getMarkers():
     places = []
@@ -99,14 +125,45 @@ def getMarkers():
         places.append(place)
     return response.json(places)
 
-#Controladores para la Lista
-
+#///////////////////////////////////////////////////////////////////////////////////////////////#
 #Controladores para Cazar
 #def randQ():
 
+def randOrd(eList):
+    random.shuffle(eList)
+    return eList
+
+def existe(lista, elemento):
+    for i in lista:
+        if(i==elemento):
+            return True
+    return False
+
+def mulCho(esc, escL):
+    lis=[esc]
+    eL=len(escL)
+    i=0;
+
+    while(i <= eL or len(lis) < 4):
+        
+        r=random.randrange(i,i+5);
+        if(r<=eL and len(lis) < 4):
+            i=r
+            print (escL[i])
+
+            if( (escL[i] != esc) and (existe(lis,escL[i])==False)):
+                lis.append(escL[i])
+        else:
+            return lis
+            
+def mulChoice(esc,escL):
+    l=mulCho(esc,escL)
+    l=randOrd(l)
+    return (l)
+
 #def correct():
 
-#def nameSculpt():
+#def mcName():
 
 #def mcMaterial():
 
